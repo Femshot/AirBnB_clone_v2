bdef7df51e446ec284616c3f8ae8de0e840d5b48
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-13 10:05:10 +0100

    Root priviledges on commands

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,7 +17,7 @@ echo '<html>
   </body>
 </html>' > /data/web_static/releases/test/index.html
 
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo sed -i "53 i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
