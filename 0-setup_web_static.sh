9766f797a75b9ba97d7315e152d5649392eb2c9b
Awodele Oluwafemi <awodelefemi@yahoo.com>
2023-09-21 00:38:10 +0100

    adjusted to directories not Directories paths

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,9 +4,9 @@
 sudo apt-get update
 sudo apt-get -y install nginx
 
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo chown -hR $USER:$USER /data/
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+sudo chown -hR $USER:$USER /data
 
 echo '<html>
   <head>
@@ -17,7 +17,7 @@ echo '<html>
   </body>
 </html>' > /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 sudo sed -i "53 i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 
